0fbcaba6776575e013d80762143c0d9de3a377a4
ayana powell <ayanaowell1@gmail.com>
2016-10-14 10:00:07 -0700

    create the method for the get request to pull user information and send to a display method

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,1 +1,10 @@
 var Github = require('./../js/github.js').githubModule;
+
+$(document).ready(function() {
+  var github = new Github();
+  $('#github_form').submit(function(event) {
+    event.preventDefault();
+    var search = $('#search').val();
+    var userInfo = github.findUser(search);
+  });
+});
--- js/github.js
+++ js/github.js
@@ -1,2 +1,24 @@
+var githubKey = require('./../.env').githubKey;
+// 9ea22165bb0152d538e4abe449c33601f712d329
+function Github() {
+}
+
+Github.prototype.findUser = function(username) {
+  var that = this;
+  var userInfo = [];
+  $.get('https://api.github.com/users/' + username + '?access_token=' + githubKey).then(function(response) {
+    var name = response.name;
+        email = response.email;
+        bio = response.bio,
+        url = response.html_url,
+        reposUrl = response.repos_url;
+    userInfo.push(name,email,bio);
+    that.displayUser(userInfo);
+  });
+};
+
+Github.prototype.displayUser = function(userInfo) {
+
+};
 
 exports.githubModule = Github;
